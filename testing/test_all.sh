21f02b3adf109b9380416c41dc8a4ff7ff1fa7ec
Donald Pinckney <donald_pinckney@icloud.com>
2016-06-25 23:18:05 -0700

    bazel

--- testing/test_all.sh
+++ testing/test_all.sh
@@ -2,10 +2,16 @@
 
 cd "$(dirname "$0")"/..
 
+exitCode=0
+
 for problemDir in testing/*/
 do
     problemDir=${problemDir%*/}
     problem=${problemDir##*/}
     echo "Testing $problem"
-    bash testing/test_problem.sh $problem
+    if ! bash testing/test_problem.sh $problem; then
+        exitCode=1
+    fi
 done
+
+exit $exitCode
