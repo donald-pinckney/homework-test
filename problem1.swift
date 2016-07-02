179153c73a01b5a791984df6980511b21403b7ca
Donald Pinckney <donald_pinckney@icloud.com>
2016-07-02 14:06:16 -0700

    Update problem1.swift

--- problem1.swift
+++ problem1.swift
@@ -1,11 +1,4 @@
 import Foundation
 
-// Uncomment to test either slower execution for timing values
-//sleep(1)
-// or to test forceful kill after too long
-sleep(1)
-let x: Int? = nil
-print(x!)
-
 // For this problem, you need to print the text "Hello, World!"
-print("Hello, World!")
+print("my text here")
