45d57aabfb11ec262bb99c8d0804af0791311980
Donald Pinckney <donald_pinckney@icloud.com>
2016-08-14 18:29:59 -0700

    Update to newer build of Swift

--- .travis.install_swift.sh
+++ .travis.install_swift.sh
@@ -5,7 +5,7 @@
 
 # Install Swift
 mkdir ~/bin
-swiftURL="https://swift.org/builds/swift-3.0-preview-1/ubuntu1404/swift-3.0-preview-1/swift-3.0-preview-1-ubuntu14.04.tar.gz"
+swiftURL="https://swift.org/builds/swift-3.0-preview-4/ubuntu1510/swift-3.0-PREVIEW-4/swift-3.0-PREVIEW-4-ubuntu15.10.tar.gz"
 swiftFile=$(basename $swiftURL)
 swiftDir="${swiftFile/.tar.gz/}"
 wget -nv $swiftURL
